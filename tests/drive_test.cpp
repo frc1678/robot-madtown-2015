e5a7c2fd6cbce428d06bd48591470528beed7e25
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2015-11-08 18:39:34 +0000

    Added drivetrain tests

--- tests/drive_test.cpp
+++ tests/drive_test.cpp
@@ -1,12 +1,12 @@
 #include <fstream>
 #include "gtest/gtest.h"
 #include "drivetrain/drive_plant.h"
+#include "drivetrain/drive_controller.h"
 #include "unitscpp/unitscpp.h"
 
 class DriveTest : public ::testing::Test {
 protected:
-  void SetUp()
-  {
+  void SetUp() {
 
   }
 
@@ -18,32 +18,38 @@ protected:
   // TODO(Kyle): Add an option for logging
   void simulate_for(Time t) {
     for (int i = 0; i < t.to(.005*s); i++) {
-      plant.update(.005*s, 0*V, 0*V);
+      Voltage left, right;
+      std::tie(left, right) = controller.update(plant.distance_left(), plant.distance_right);
+      plant.update(.005*s, left, right);
     }
   }
 
   // TODO (Kelly): Figure out naming conventions. Are we using google style guide?
   // (_ after member variables)
   DrivePlant plant;
+  DriveController controller;
 };
 
 TEST_F(DriveTest, Sanity) {
   // Sanity check
   Time a = 2.5*s;
   Time b = 10*s;
-    for (int i = 0; i < a.to(.005*s); i++) {
-      plant.update(.005*s, 0*V, 0*V);
-    }
-    for (int i = 0; i < b.to(.005*s); i++) {
-      plant.update(.005*s, 0*V, 0*V);
-    }
-EXPECT_NEAR(0,plant.velocity_right().to(m/s), 0.01);  
-EXPECT_NEAR(0,plant.velocity_left().to(m/s), 0.01);
+  for (int i = 0; i < a.to(.005*s); i++) {
+    plant.update(.005*s, 0*V, 0*V);
+  }
+  for (int i = 0; i < b.to(.005*s); i++) {
+    plant.update(.005*s, 0*V, 0*V);
+  }
+  EXPECT_NEAR(0,plant.velocity_right().to(m/s), 0.01);  
+  EXPECT_NEAR(0,plant.velocity_left().to(m/s), 0.01);
 }
 
 TEST_F(DriveTest, DistDriven){
-
-
+  controller.set_distance_goal(10*ft);
+  simulate_for(10*s);
+  EXPECT_NEAR(0, plant.velocity_right().to(m/s), 0.01);
+  EXPECT_NEAR(0, plant.velocity_left().to(m/s), 0.01);
+  EXPECT_NEAR(10, plant.distance().to(ft), 0.1); 
 }
 
 //TODO (Jasmine): Set this in a separate file so we can run tests for all
